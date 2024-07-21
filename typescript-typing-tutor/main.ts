844e6761291db562821867b67ab221a99b72a73a
Erin Corcoran <corcoran.erin.a@gmail.com>
2024-07-21 15:04:20 +0000

    Completed typescript-typing-tutor exercise.

---
+++ typescript-typing-tutor/main.ts
@@ -0,0 +1,27 @@
+const $spanElements = document.querySelectorAll('span');
+if (!$spanElements) throw new Error('span elements do not exist');
+
+let $currentIndex = 0;
+const $totalLength = $spanElements.length - 1;
+
+document.addEventListener('keydown', (event: KeyboardEvent) => {
+  if (
+    event.key === $spanElements[$currentIndex].textContent &&
+    $currentIndex < $totalLength
+  ) {
+    $spanElements[$currentIndex].className = 'typed-correctly';
+    $spanElements[$currentIndex + 1].className = 'next-letter';
+    $currentIndex++;
+  } else if (
+    event.key === $spanElements[$currentIndex].textContent &&
+    $currentIndex === $totalLength
+  ) {
+    $spanElements[$currentIndex].className = 'typed-correctly';
+  } else if (
+    event.key !== $spanElements[$currentIndex].textContent &&
+    $currentIndex <= $totalLength
+  ) {
+    $spanElements[$currentIndex].className = 'typed-incorrectly';
+  }
+  return $currentIndex;
+});
